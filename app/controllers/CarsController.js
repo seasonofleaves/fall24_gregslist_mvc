37c19dac798960a1af1136eafb6da24479ead490
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-28 12:13:34 -0600

    added car to appstate

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,14 +1,19 @@
 import { AppState } from "../AppState.js";
+import { carsService } from "../services/CarsService.js";
 import { getFormData } from "../utils/FormHandler.js";
 
 export class CarsController {
   constructor() {
     console.log('Cars controller is loaded 🚗');
+    AppState.on('cars', this.drawCars)
+
+
     this.drawCars()
   }
 
   drawCars() {
     const cars = AppState.cars
+    console.log('cars in the appstate', cars);
     let carHTMLContent = ''
     cars.forEach(car => carHTMLContent += car.cardHTMLTemplate)
     const carsElem = document.getElementById('car-listings')
@@ -23,6 +28,8 @@ export class CarsController {
     // NOTE getFormData will pull all values out of named inputs and construct an object for you
     const carDataFromForm = getFormData(form)
     console.log('car data from form', carDataFromForm);
+
+    carsService.createCar(carDataFromForm)
 
   }
 }
--- app/services/CarsService.js
+++ app/services/CarsService.js
@@ -1,3 +1,13 @@
-class CarsService { }
+import { AppState } from "../AppState.js"
+import { Car } from "../models/Car.js"
+
+class CarsService {
+  createCar(carFormData) {
+    const cars = AppState.cars
+    // make sure we turn the POJO from the form into a Car before it goes into the appstate
+    const newCar = new Car(carFormData)
+    cars.push(newCar)
+  }
+}
 
 export const carsService = new CarsService()
