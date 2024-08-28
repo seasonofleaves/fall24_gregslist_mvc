c26539fde2bda59690fb50d501b055177eedf358
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-28 11:59:19 -0600

    got car form data

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,4 +1,5 @@
 import { AppState } from "../AppState.js";
+import { getFormData } from "../utils/FormHandler.js";
 
 export class CarsController {
   constructor() {
@@ -12,5 +13,16 @@ export class CarsController {
     cars.forEach(car => carHTMLContent += car.cardHTMLTemplate)
     const carsElem = document.getElementById('car-listings')
     carsElem.innerHTML = carHTMLContent
+  }
+
+  createCar() {
+    event.preventDefault() // do not refresh the page
+    console.log('creating a car!');
+    const form = event.target
+    console.log('Form that just sumbitted', form);
+    // NOTE getFormData will pull all values out of named inputs and construct an object for you
+    const carDataFromForm = getFormData(form)
+    console.log('car data from form', carDataFromForm);
+
   }
 }
