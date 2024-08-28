4e023e2ca7691be9c19262027fd6c5fc20d58df3
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-28 09:53:56 -0600

    added cars controller

--- app/router-config.js
+++ app/router-config.js
@@ -1,3 +1,4 @@
+import { CarsController } from "./controllers/CarsController.js";
 import { ExamplesController } from "./controllers/ExamplesController.js";
 import { HomeController } from "./controllers/HomeController.js";
 import { Router } from "./utils/Router.js";
@@ -6,14 +7,17 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
   {
     path: '',
+    controllers: [HomeController],
     view: 'app/views/HomeView.html'
   },
   {
     path: '#/about',
+    controllers: [ExamplesController],
     view: 'app/views/AboutView.html'
   },
   {
     path: '#/cars',
+    controllers: [CarsController],
     view: 'app/views/CarsView.html'
   }
 ])
